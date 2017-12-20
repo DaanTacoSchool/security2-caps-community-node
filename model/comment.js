c71345f621ffc2da075490b2052f0641f367eaf2
TolgaElbir <telbir@student.avans.nl>
2017-12-20 10:48:37 +0100

    hotfix

--- model/comment.js
+++ model/comment.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const User = require('./user')
+const User = require('./user');
 
 
 const CommentSchema = new Schema({
   postId: String,
   content: String,
-  user: User,
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
 });
 
 
 
 const Comment = mongoose.model('comment', CommentSchema);
-
+const comment =  new Comment({
+    postId: '5a3a1d60969f092f7c462c12',
+    content: 'test',
+    user: '5a3a1d60969f092f7c462c12',
+});
 module.exports = Comment;
--- model/post.js
+++ model/post.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const User = mongoose.model('user');
 const Like = mongoose.model('like');
+const Comment = mongoose.model('comment');
 
 const PostSchema = new Schema({
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
-    like: {type: mongoose.Schema.Types.ObjectId, ref: 'like'},
+    like: [{type: mongoose.Schema.Types.ObjectId, ref: 'like'}],
+    comment: [{type: mongoose.Schema.Types.ObjectId, ref: 'comment'}],
     made_by: String,
     title: String,
     description: {
@@ -28,15 +30,24 @@ const user = new User({
 const like = new Like({
     userId: 'String',
     likes: 5,
-})
+});
+
+const comment =  new Comment({
+    postId: '5a3a1d60969f092f7c462c12',
+    content: 'test',
+    user: this.user,
+});
 
 var users = [user];
 const post = new Post({
+    title: 'Caps Sweater',
     description: 'Check out my new sweater!',
+    made_by: 'Tom',
     imagePath: 'https://www.fjallraven.nl/media/catalog/product/cache/all/base/522x/17f82f742ffe127f42dca9de82fb58b1/F/8/F89941-220_0.jpg',
-    user: user,
-    Like: like,
-})
-.save();
+    comments: [comment],
+    user: User,
+    like: [like],
+});
+// .save();
 
 module.exports = Post;
