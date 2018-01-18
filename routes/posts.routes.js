ad623a4bacf9cbfea529a31fdccc0e6694a4fadf
Rick Lambrechts <rick@lambrechts.nu>
2018-01-18 21:12:40 +0100

    Updated post and like route

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -20,6 +20,20 @@ router.get('/posts', function(req, res) {
         });
 });
 
+router.get('/posts/u', jwt({
+        secret: config.secretKey,
+        credentialsRequired: true
+    }), (req, res) => {
+        Post.find({ 'user.guid': req.user.sub})
+            .populate('comments')
+            .populate('likes')
+            .then((posts) => {
+                res.status(200).json(posts);
+            }).catch((error) => {
+                res.status(400).json(error);
+            });
+});
+
 // Get a post by ID
 router.get('/posts/:id', jwt({
         secret: config.secretKey,
@@ -106,24 +120,6 @@ router.delete('/posts/:id', function(req, res) {
         .catch((error) => {
             res.status(400).json({error: 'Could not delete post'})
         });
-
-        router.get('/posts/u', jwt({
-          secret: config.secretKey,
-          credentialsRequired: true
-        }), (req, res) => {
-          Post.find({ 'user.guid': req.user.sub})
-                      .populate({
-                          path: 'post',
-                          populate: ['post', 'comments']
-                      })
-                      .then((posts) => {
-                          console.log(posts);
-                          res.status(200).json(posts);
-                      }).catch((error) => {
-                          res.status(400).json(error);
-                      });
-        });
-
 });
 
 
